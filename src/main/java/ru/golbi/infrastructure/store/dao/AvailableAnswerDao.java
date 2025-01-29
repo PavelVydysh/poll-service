482e0e5b6e1c322c863af12de9c4e0624d49ae38
Pavel Vydysh <vydysh.pavel@gmail.com>
2025-01-29 23:45:06 +0300

    [Back] [poll-service] (2) edit repository to save poll version

--- src/main/java/ru/golbi/infrastructure/store/dao/AvailableAnswerDao.java
+++ src/main/java/ru/golbi/infrastructure/store/dao/AvailableAnswerDao.java
@@ -1,9 +1,20 @@
 package ru.golbi.infrastructure.store.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.golbi.infrastructure.store.entity.AvailableAnswerEntity;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AvailableAnswerDao extends CrudRepository<AvailableAnswerEntity, UUID> {
+
+    @Query(
+            "SELECT av FROM " + AvailableAnswerEntity.ENTITY_NAME + " av " +
+            "WHERE av." + AvailableAnswerEntity.AVAILABLE_ANSWER_ID_FIELD_NAME + " IN :ids"
+    )
+    List<AvailableAnswerEntity> findAllByIds(List<UUID> ids);
+
+
+
 }
--- src/main/java/ru/golbi/infrastructure/store/repository/PollVersionJpaRepository.java
+++ src/main/java/ru/golbi/infrastructure/store/repository/PollVersionJpaRepository.java
@@ -2,11 +2,14 @@ package ru.golbi.infrastructure.store.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
 import ru.golbi.domain.model.PollVersion;
 import ru.golbi.domain.repository.PollVersionRepository;
 import ru.golbi.domain.wrapper.ResultWrapper;
 import ru.golbi.infrastructure.store.converter.AvailableAnswerConverter;
 import ru.golbi.infrastructure.store.converter.PollVersionConverter;
+import ru.golbi.infrastructure.store.dao.AvailableAnswerDao;
 import ru.golbi.infrastructure.store.dao.PollVersionAndAvailableAnswerDao;
 import ru.golbi.infrastructure.store.dao.PollVersionDao;
 import ru.golbi.infrastructure.store.entity.AvailableAnswerEntity;
@@ -22,9 +25,11 @@ import java.util.UUID;
 public class PollVersionJpaRepository implements PollVersionRepository {
 
     private final PollVersionDao pollVersionDao;
+    private final AvailableAnswerDao availableAnswerDao;
     private final PollVersionAndAvailableAnswerDao pollVersionAndAvailableAnswerDao;
 
     @Override
+    @Transactional
     public ResultWrapper<Void> savePollVersion(UUID pollId, PollVersion pollVersion) {
         PollVersionEntity pollVersionEntity = PollVersionConverter.toPollVersionEntity(pollVersion);
         List<AvailableAnswerEntity> availableAnswerEntities = AvailableAnswerConverter.toListAvailableAnswerEntity(
@@ -36,23 +41,43 @@ public class PollVersionJpaRepository implements PollVersionRepository {
         pollVersionEntity.setPollId(pollId);
         pollVersionEntity.setNew(true);
 
-        List<PollVersionAndAvailableAnswerEntity> pollVersionAndAvailableAnswerEntities = new ArrayList<>();
-        for (AvailableAnswerEntity availableAnswerEntity : availableAnswerEntities) {
-            UUID availableAnswerId = UUID.randomUUID();
-            availableAnswerEntity.setAvailableAnswerId(availableAnswerId);
-            availableAnswerEntity.setNew(true);
+        pollVersionDao.save(pollVersionEntity);
 
+        List<PollVersionAndAvailableAnswerEntity> pollVersionAndAvailableAnswerEntities = new ArrayList<>();
+        List<UUID> transmittedAvailableAnswerIds = new ArrayList<>();
+        List<AvailableAnswerEntity> availableAnswerEntitiesToSave = new ArrayList<>();
+
+        for (AvailableAnswerEntity availableAnswerEntity : availableAnswerEntities) {
+            UUID availableAnswerId = availableAnswerEntity.getAvailableAnswerId();
+            if (ObjectUtils.isEmpty(availableAnswerId)) {
+                availableAnswerEntity.setAvailableAnswerId(
+                        availableAnswerId = UUID.randomUUID()
+                );
+                availableAnswerEntity.setNew(true);
+                availableAnswerEntitiesToSave.add(availableAnswerEntity);
+            } else {
+                transmittedAvailableAnswerIds.add(availableAnswerId);
+            }
+        }
+
+        List<AvailableAnswerEntity> allAvailableAnswerEntities = availableAnswerDao.findAllByIds(
+                transmittedAvailableAnswerIds
+        );
+
+        availableAnswerDao.saveAll(availableAnswerEntitiesToSave);
+
+        allAvailableAnswerEntities.addAll(availableAnswerEntitiesToSave);
+
+        for (AvailableAnswerEntity availableAnswerEntity : allAvailableAnswerEntities) {
             PollVersionAndAvailableAnswerEntity pollVersionAndAvailableAnswerEntity = new PollVersionAndAvailableAnswerEntity();
             pollVersionAndAvailableAnswerEntity.setPollVersionAndAvailableAnswerId(UUID.randomUUID());
             pollVersionAndAvailableAnswerEntity.setPollVersionId(pollVersionEntityId);
-            pollVersionAndAvailableAnswerEntity.setAvailableAnswerId(availableAnswerId);
+            pollVersionAndAvailableAnswerEntity.setAvailableAnswerId(availableAnswerEntity.getAvailableAnswerId());
             pollVersionAndAvailableAnswerEntity.setNew(true);
 
             pollVersionAndAvailableAnswerEntities.add(pollVersionAndAvailableAnswerEntity);
         }
 
-        pollVersionDao.save(pollVersionEntity);
-//        availableAnswerDao.saveAll(availableAnswerEntities);
         pollVersionAndAvailableAnswerDao.saveAll(pollVersionAndAvailableAnswerEntities);
 
         pollVersionEntity.setPollVersionsAndAvailableAnswers(pollVersionAndAvailableAnswerEntities);
